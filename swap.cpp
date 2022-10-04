73adc31c9a02c8ea43015d290eb2357b1a2de118
Sai Kiran Ippili <iamsaikiranippili@gmail.com>
2022-10-04 15:49:56 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void swap(int *first, int *second)
+{
+    int temp;
+    temp = *first;
+    *first = *second;
+    *second = temp;
+}
+int main()
+{
+    int a, b;
+    cout << "Enter the numbers : " << endl;
+    cin >> a;
+    cin >> b;
+    cout << "Before swap, a = " << a << ", b = " << b << endl;
+    swap(&a, &b);
+    cout << "After swap, a = " << a << ", b = " << b << endl;
+}
