74b4ae4ff7f44322a320a44095fec63f80e15b6a
Monika Basene <113535275+Monika3002@users.noreply.github.com>
2022-10-07 13:33:02 +0530

    Update LetterCount.cpp

--- LetterCount.cpp
+++ LetterCount.cpp
@@ -1,39 +1,21 @@
 #include <iostream> 
 
 using namespace std; 
-
-  
-
 int countLetters(string s) 
-
 { 
-
-    int res = 0; 
-
-  
-
-    for (int i=0; i<s.length(); i++) 
-
-        if (s[i] != ' ') 
-
-            res++; 
-
-  
-
-    return res; 
-
+  int res = 0; 
+   for (int i=0; i<s.length(); i++)
+   {
+     if (s[i] != ' ') 
+       res++;
+   }
+   return res; 
 } 
-
-  
-
 int main() 
-
 { 
-
-    string str = "w3resource "; 
-
-    cout << countLetters(str); 
-
-    return 0; 
-
+   string str; 
+  cout<<"Write something here : ";
+  getline( cin,str);
+   cout << countLetters(str); 
+   return 0; 
 } 
